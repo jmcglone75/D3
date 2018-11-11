3e2be266f18647ff356a5805caa991562dde7a48
Jake Mcglone <j.mcglone@localhost.localdomain>
2018-11-11 00:33:45 -0500

    header of table now being generated

--- truth.rb
+++ truth.rb
@@ -26,14 +26,39 @@ get '/display' do
 
 	#check validity of params
 	if params[:true].length > 1 or params[:false].length > 1 or params[:size].to_i < 2 or params[:true] == params[:false]
-		return erb :error
+		return erb :invalid
 	end
-	
 
-	
+	#make array of true/false values
+	#loop 2^n times (for each boolean expression of props)
+	#loop n times (to determine and/or/xor)
+	table = Array.new
+	n = params[:size].to_i
+
+	(2**n).times do |i|
+		expression = Array.new
+		logical_and = true
+		logical_or = false
+		logical_xor = false
+
+		# permute truth/false values for each expression
+		n.times do |j|
+			if((i/(2**(n-j-1)))%2 == 1)
+				logical_or = true
+				logical_xor = !logical_xor
+				expression << true
+			else
+				logical_and = false
+				expression << false
+			end
+		end
+		expression << logical_and
+		expression << logical_or
+		expression << logical_xor
+		table << expression
+	end
 		
-	erb :display, :locals => { tru: params[:true], fls: params[:false], size: params[:size] }
-
+	erb :display, :locals => { tru: params[:true], fls: params[:false], size: params[:size], table: table}
 end
 
 # If we have an error
